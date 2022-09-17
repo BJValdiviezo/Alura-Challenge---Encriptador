a092fe7d0ab34aee82366d4409c801f93f2ce5de
Bjvaldiviezo <bjvaldiviezo@gmail.com>
2022-09-17 17:27:44 -0300

    Modificaciones en responsividad

--- js/buttonCopy.js
+++ js/buttonCopy.js
@@ -2,8 +2,10 @@ const btnCopy = document.querySelector(".copy-button");
 btnCopy.addEventListener("click", (event)=>{
     event.preventDefault();
     const output =document.querySelector("#text-encryter-or-decrypted")
+    const input = document.querySelector("#text")
     output.select();
     document.execCommand('copy');
-    output.value="";
-    btnCopy.textContent = "El mensaje fue copiado con Exito"
+    output.value= "";
+    input.value= "";
+    btnCopy.textContent = "El mensaje fue copiado con Exito";
 });
